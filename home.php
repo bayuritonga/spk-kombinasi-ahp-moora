216b85c49e62562e4df45aad7b3315191749890e
Bayu Surbana Ritonga <bayuciigokiel@gmail.com>
2025-01-29 06:54:11 +0700

    Update

---
+++ home.php
@@ -0,0 +1,30 @@
+<div class="page-header">
+	<h1>SPK Metode AHP dan MOORA</h1>
+</div>
+<p>Sistem Pendukung Keputusan (Decision Support System, DSS) adalah sebuah sistem
+	komputer yang dirancang untuk membantu pengguna dalam membuat keputusan dengan
+	menyediakan informasi yang diperlukan dan menganalisisnya sesuai dengan kebutuhan pengguna.
+	DSS dapat membantu pengguna dengan menyediakan data, informasi, dan analisis yang
+	diperlukan untuk membuat keputusan yang tepat. DSS biasanya digunakan dalam situasi
+	yang tidak terstruktur atau tidak dapat diprediksi dengan pasti, di mana pengguna
+	membutuhkan bantuan dalam mengelola informasi dan membuat keputusan berdasarkan
+	data yang tersedia.</p>
+<p>Analytical Hierarchy Process (AHP) merupakan suatu metode teknik terstruktur
+	yang biasa digunakan dalam sistem pengambilan keputusan. AHP didesain untuk
+	menangani secara rasional untuk dapat memilih alternatif yang paling baik diantara
+	semua alternatif yang ada terhadap sejumlah kriteria tertentu. Sehubungan didalam
+	melakukan metode pengambilan keputusan ini maka orang yang melakukan pengambilan
+	keputusan membuat asesmen dengan menggunakan perbandingan yang sederhana yang
+	kemudian dimanfaatkan untuk menentukan prioritas ratarata untuk melakukan
+	perangkingan terhadap alternatif keputusan. AHP juga mengakomodasi inkosistensi
+	penilaian dan cara untuk meningkatkan konsistensi tersebut.</p>
+<p>Multi-Objective Optimization by Ratio Analysis (MOORA) adalah salah satu metode
+	yang dapat digunakan untuk membantu proses pengambilan keputusan dalam SPK.
+	Metode MOORA dikembangkan pertama kali oleh Brauers yang menerapkannya dalam
+	proses pengambilan keputusan dengan multikriteria. Kelebihan metode ini salah
+	satunya adalah fleksibilitas yang tinggi dan tingkat selektifitas yang baik.
+	Hal ini disebabkan MOORA mampu menentukan tujuan dari kriteria yang saling
+	bertolak belakang, dimana kriteria dapat bernilai menguntungkan (benefit)
+	atau yang tidak menguntungkan (cost). Selain itu, MOORA juga memiliki kemampuan
+	memisahkan unsur subjektif dari suatu proses evaluasi secara mudah ke dalam
+	kriteria bobot keputusan yang memiliki beberapa atribut pengambil keputusan.</p>
